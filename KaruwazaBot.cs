2fe04d3050773eddb9136de2f0f05752acb4708a
baonhatntt2016-lab <baonhatntt2016@gmail.com>
2026-03-09 09:32:35 +0700

    Update KaruwazaBot.cs

--- KaruwazaBot.cs
+++ KaruwazaBot.cs
@@ -1,24 +1,72 @@
 using Microsoft.Web.WebView2.WinForms;
 using System.Threading.Tasks;
 
-public class KaruwazaBot
+namespace KaruwazaDriver
 {
-    private WebView2 web;
-
-    public KaruwazaBot(WebView2 webView)
+    public class KaruwazaBot
     {
-        web = webView;
-    }
+        private WebView2 web;
+        private bool _isStarted = false;
 
-    public async Task RunQuery()
-    {
-        await web.ExecuteScriptAsync("fSubmit(11,true);");
-    }
+        public KaruwazaBot(WebView2 webView)
+        {
+            web = webView;
+        }
 
-    public async Task<string> GetTable()
-    {
-        return await web.ExecuteScriptAsync(@"
-            document.querySelector('table').outerHTML
-        ");
+        public async Task Run()
+        {
+            if (_isStarted) return;
+            _isStarted = true;
+
+            var url = web.Source.ToString();
+
+            if (url.Contains("kwzweb"))
+            {
+                await WaitForElement("#txtLoginUserid");
+
+                await web.ExecuteScriptAsync(@"
+                    document.getElementById('txtLoginUserid').value = 'tnqthai@organvn.com.vn';
+                    document.getElementById('txtLoginPassword').value = 'tnqthai0439';
+                    document.forms[0].submit();
+                ");
+            }
+
+            await WaitForElement("#pJDbTitle");
+
+            await web.ExecuteScriptAsync(@"
+                let ddl = document.getElementById('pJDbTitle');
+                ddl.value = '14';
+                ddl.dispatchEvent(new Event('change', { bubbles: true }));
+            ");
+
+            await WaitForElement("body");
+
+            await web.ExecuteScriptAsync("fJyokenNew();");
+
+            await Task.Delay(3000);
+
+            await web.ExecuteScriptAsync("fTableSelect(1014);");
+
+            await Task.Delay(3000);
+
+            await web.ExecuteScriptAsync("fSubmit(11,true);");
+
+            await Task.Delay(3000);
+        }
+
+        private async Task WaitForElement(string selector)
+        {
+            while (true)
+            {
+                var result = await web.ExecuteScriptAsync($@"
+                    document.querySelector('{selector}') != null
+                ");
+
+                if (result.Contains("true"))
+                    return;
+
+                await Task.Delay(500);
+            }
+        }
     }
 }
